437b824ec9d496815053ffd29d24b8cac071779c
guoyikai <1652037413@qq.com>
2024-09-25 01:39:58 +0800

    完成首页列表框架

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -9,11 +9,24 @@ import {useLocalStore} from "mobx-react";
 import HomeStore from "./HomeStore";
 import {observer} from "mobx-react";
 
+import icon_heart from '../../assets/icon_heart.png'
+import icon_heart_empty from '../../assets/icon_heart_empty.png'
+
 const {width: SCREEN_WIDTH, height} = Dimensions.get('window');
+
 
 export default observer(() => {
 
     const store = useLocalStore(() => new HomeStore())
+
+    const refreshNewData = ()=>{
+        store.resetPage()
+        store.requestHomeList();
+    }
+
+    const loadMoreData=()=>{
+        store.requestHomeList();
+    }
 
     useEffect(() => {
         store.requestHomeList();
@@ -25,10 +38,18 @@ export default observer(() => {
                 <Image source={{uri: item.image}} style={styles.itemImg}/>
                 <Text style={styles.titleTxt}>{item.title}</Text>
                 <View style={styles.nameLayout}>
-                    <Image style={styles.avatarImg} source={{uri:item.avatarUrl}}/>
+                    <Image style={styles.avatarImg} source={{uri: item.avatarUrl}}/>
                     <Text style={styles.nameTxt}>{item.userName}</Text>
+                    <Image style={styles.heart} source={icon_heart_empty}/>
+                    <Text style={styles.countTxt}>{item.favoriteCount}</Text>
                 </View>
             </View>
+        )
+    }
+
+    const Footer = ()=>{
+        return (
+            <Text style={styles.footerTxt}>没有更多数据</Text>
         )
     }
 
@@ -38,7 +59,13 @@ export default observer(() => {
                       data={store.homeList}
                       renderItem={renderItem}
                       numColumns={2}
-                      contentContainerStyle={styles.container}>
+                      extraData={[store.refreshing]}
+                      contentContainerStyle={styles.container}
+                      refreshing={store.refreshing}
+                      onEndReachedThreshold={0.1}
+                      onEndReached={loadMoreData}
+                      onRefresh={refreshNewData}
+            ListFooterComponent={Footer}>
             </FlatList>
         </View>
     )
@@ -78,22 +105,41 @@ const styles = StyleSheet.create({
         marginHorizontal: 10,
         marginVertical: 4
     },
-    nameLayout:{
+    nameLayout: {
         width: '100%',
         flexDirection: 'row',
         alignItems: 'center',
-        paddingHorizontal:10,
-        marginBottom:10
+        paddingHorizontal: 10,
+        marginBottom: 10
     },
     avatarImg: {
-        width:20,
-        height:20,
+        width: 20,
+        height: 20,
         resizeMode: 'cover',
-        borderRadius:10,
+        borderRadius: 10,
     },
-    nameTxt:{
-        fontSize:12,
-        color:'#999',
+    nameTxt: {
+        fontSize: 12,
+        color: '#999',
         marginLeft: 6,
+        flex: 1
+    },
+    heart: {
+        width: 20,
+        height: 20,
+        resizeMode: 'contain'
+    },
+    countTxt: {
+        fontSize: 14,
+        color: '#999',
+        marginLeft: 4,
+    },
+    footerTxt:{
+        width:'100%',
+        fontSize: 14,
+        color: '#999',
+        marginVertical:16,
+        textAlign:'center',
+        textAlignVertical:'center'
     }
 })
--- src/modules/home/HomeStore.ts
+++ src/modules/home/HomeStore.ts
@@ -1,27 +1,54 @@
 import {request} from "../../utils/request";
-import {makeAutoObservable, observable} from "mobx";
+import {action, makeAutoObservable, observable} from "mobx";
 
 const SIZE = 10;
 export default class HomeStore {
 
     page: number = 1
+
     constructor() {
         makeAutoObservable(this);
     }
 
-   @observable homeList: ArticleSimple[] = []
+    @observable homeList: ArticleSimple[] = []
+
+    @observable refreshing: boolean = false;
+
+    @action
+    resetPage = ()=>{
+        this.page = 1
+    }
 
     requestHomeList = async () => {
+        if (this.refreshing) {
+            return
+        }
         try {
+            this.refreshing = true
             const params = {
                 page: this.page,
                 size: SIZE
-            }
+            };
             const {data} = await request('homeList', params)
             console.log(`data = ${JSON.stringify(data)}`)
-            this.homeList = data
+            if (data?.length) {
+                if (this.page === 1) {
+                    this.homeList = data
+                } else {
+                    this.homeList = [...this.homeList, ...data]
+                }
+                this.page = this.page + 1
+            } else {
+                if (this.page === 1) {
+                    this.homeList = []
+                } else {
+                    //已经加载玩了，没有更多数据了
+                }
+            }
         } catch (error) {
             console.log(error)
+        }finally {
+            this.refreshing = false
         }
     }
 }
