901b59a978c56fa49b033517919cdae27363d425
guoyikai <1652037413@qq.com>
2024-09-25 01:10:12 +0800

    首页数据开发一半

--- src/modules/home/Home.tsx
+++ src/modules/home/Home.tsx
@@ -2,13 +2,16 @@ import React, {useEffect} from "react";
 import {
     View,
     Image,
-    StyleSheet, Text,
+    StyleSheet, Text, FlatList, Dimensions
 } from 'react-native'
 
 import {useLocalStore} from "mobx-react";
 import HomeStore from "./HomeStore";
+import {observer} from "mobx-react";
 
-export default () => {
+const {width: SCREEN_WIDTH, height} = Dimensions.get('window');
+
+export default observer(() => {
 
     const store = useLocalStore(() => new HomeStore())
 
@@ -16,12 +19,30 @@ export default () => {
         store.requestHomeList();
     }, []);
 
+    const renderItem = ({item, index}: { item: ArticleSimple, index: number }) => {
+        return (
+            <View style={styles.item}>
+                <Image source={{uri: item.image}} style={styles.itemImg}/>
+                <Text style={styles.titleTxt}>{item.title}</Text>
+                <View style={styles.nameLayout}>
+                    <Image style={styles.avatarImg} source={{uri:item.avatarUrl}}/>
+                    <Text style={styles.nameTxt}>{item.userName}</Text>
+                </View>
+            </View>
+        )
+    }
+
     return (
         <View style={styles.root}>
-
+            <FlatList style={styles.flatList}
+                      data={store.homeList}
+                      renderItem={renderItem}
+                      numColumns={2}
+                      contentContainerStyle={styles.container}>
+            </FlatList>
         </View>
     )
-}
+})
 
 const styles = StyleSheet.create({
     root: {
@@ -29,5 +50,50 @@ const styles = StyleSheet.create({
         height: '100%',
         justifyContent: 'center',
         alignItems: 'center',
+        backgroundColor: '#f0f0f0',
+    },
+    flatList: {
+        width: '100%',
+        height: '100%',
+    },
+    item: {
+        width: SCREEN_WIDTH - 18 >> 1,
+        backgroundColor: 'white',
+        marginLeft: 6,
+        marginBottom: 6,
+        borderRadius: 8,
+        overflow: 'hidden'
+    },
+    container: {
+        paddingTop: 6,
+    },
+    itemImg: {
+        width: '100%',
+        height: 250,
+        resizeMode: 'cover',
+    },
+    titleTxt: {
+        fontSize: 14,
+        color: "#333",
+        marginHorizontal: 10,
+        marginVertical: 4
+    },
+    nameLayout:{
+        width: '100%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        paddingHorizontal:10,
+        marginBottom:10
+    },
+    avatarImg: {
+        width:20,
+        height:20,
+        resizeMode: 'cover',
+        borderRadius:10,
+    },
+    nameTxt:{
+        fontSize:12,
+        color:'#999',
+        marginLeft: 6,
     }
 })
--- src/modules/home/HomeStore.ts
+++ src/modules/home/HomeStore.ts
@@ -1,9 +1,15 @@
 import {request} from "../../utils/request";
+import {makeAutoObservable, observable} from "mobx";
 
 const SIZE = 10;
 export default class HomeStore {
 
     page: number = 1
+    constructor() {
+        makeAutoObservable(this);
+    }
+
+   @observable homeList: ArticleSimple[] = []
 
     requestHomeList = async () => {
         try {
@@ -13,6 +19,7 @@ export default class HomeStore {
             }
             const {data} = await request('homeList', params)
             console.log(`data = ${JSON.stringify(data)}`)
+            this.homeList = data
         } catch (error) {
             console.log(error)
         }
